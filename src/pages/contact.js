0a6646cd404988672b1e91cbb3703a14a232c9a0
Kelvin Brito <britok30@gmail.com>
2020-04-28 23:40:56 -0400

    new pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,1 +1,12 @@
-import 
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>Contact details</p>
+    </div>
+  )
+}
+
+export default ContactPage
